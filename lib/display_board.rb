e738a0a30212494a770542a4aa5f39742e9d671f
Jason McGee <jasondmcgee17@gmail.com>
2018-10-24 22:46:34 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,10 @@
-def display_board
-  print "A Tic Tac Toe Board"
-  print "   |   |   "
-  print "-----------"
-  print "   |   |   "
-  print "-----------"
-  print "   |   |   "
+def display_board:
+  puts "A Tic Tac Toe Board"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
   # Define a method display_board that prints a 3x3 Tic Tac Toe Boa
 end
 display_board
